19ad981f6dfb01824f87296db7f9085e8da62d42
anasyakubu <yakubuanas04@gmail.com>
2024-02-09 22:16:07 +0100

    Changes

--- app/Dashboard/page.tsx
+++ app/Dashboard/page.tsx
@@ -1,7 +1,11 @@
+import Nav from "../components/Navbar/Navbar";
+// import { StickyNavbar } from "../components/Navbar/Navbar";
+
 const Dashboard = () => {
   return (
     <div>
       <h1>Dashboard</h1>
+      <Nav />
     </div>
   );
 };
