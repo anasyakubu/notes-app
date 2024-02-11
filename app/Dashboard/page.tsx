009b6f765f333ba7fa8842adbcde0dcac5b2fd00
anasyakubu <yakubuanas04@gmail.com>
2024-02-11 02:34:07 +0100

    Added a new Nav bar

--- app/Dashboard/page.tsx
+++ app/Dashboard/page.tsx
@@ -1,12 +1,12 @@
 // import Nav from "../components/Navbar/Navbar";
-// import Navbar from "../components/Navbar/Navbar";
+import Navbar from "../components/Navbar/Navbar";
 // import { StickyNavbar } from "../components/Navbar/Navbar";
 
 const Dashboard = () => {
   return (
     <div>
       <h1>Dashboard</h1>
-      {/* <Navbar /> */}
+      <Navbar />
     </div>
   );
 };
